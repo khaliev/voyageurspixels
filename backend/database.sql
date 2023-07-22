7d9fd6f8a5af11c42367bcd42cf49e401f79841d
khaliev <dev.adlan@icloud.com>
2023-07-22 21:04:35 +0200

    web app is now responsive

--- backend/database.sql
+++ backend/database.sql
@@ -10,8 +10,8 @@ INSERT INTO Shots (originalTitle, original, thumbnail) VALUES
 ('Empy street', 'https://unsplash.com/photos/j2zmqd24muQ/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjg5ODU2MzgwfA&force=true&w=1920', 'https://live.staticflickr.com/65535/53060548771_8d0261332f_o_d.jpg'),
 ('Cloudy sky', 'https://unsplash.com/photos/v5tf4xSZSO0/download?ixid=M3wxMjA3fDB8MXxhbGx8MTAwfHx8fHx8Mnx8MTY4OTg1NzUxNnw&force=true&w=1920', 'https://live.staticflickr.com/65535/53060735469_3931beab17_o_d.jpg'),
 ('Landscape', 'https://unsplash.com/photos/qpZaHxUFMCI/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjg5ODU3NDA0fA&force=true&w=1920', 'https://live.staticflickr.com/65535/53060735429_21a228044a_o_d.jpg'),
-('Landscape Marne', 'https://unsplash.com/photos/-O-eHP72aqQ/download?force=true&w=1920', 'https://live.staticflickr.com/65535/53059968097_fc1ff49e6b_o_d.jpg'),
-('Paris street', 'https://unsplash.com/photos/nokuzeWj0gw/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjg5ODU3MzI2fA&force=true&w=1920', 'https://live.staticflickr.com/65535/53060937280_94aefd9c31_o_d.jpg'),
+('Caucasus mountains', 'https://unsplash.com/photos/F5m81gkjfXs/download?ixid=M3wxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2OTAwMDEwNzB8&force=true&w=1920', 'https://live.staticflickr.com/65535/53062788414_634e01eefa_o_d.jpg'),
+('Reims street', 'https://unsplash.com/photos/QvYb-l8fBWg/download?ixid=M3wxMjA3fDB8MXxhbGx8ODN8fHx8fHwyfHwxNjg5OTk5NDM2fA&force=true&w=1920', 'https://live.staticflickr.com/65535/53062964230_377130cc60_o_d.jpg'),
 ('Sky', 'https://unsplash.com/photos/lD3tPh9o7Hc/download?ixid=M3wxMjA3fDB8MXxhbGx8ODJ8fHx8fHwyfHwxNjg5ODU3NTEyfA&force=true&w=1920', 'https://live.staticflickr.com/65535/53061034763_d5367e6e96_o_d.jpg');
 
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ function Footer() {
     <div className="footer">
       <div className="top">
         <h1>© Voyageur's Pixels</h1>
-      </div>
-      <div className="socialmedia">
-        <a href="/">
-          <i className="fa-brands fa-facebook-square" />
-        </a>
-        <a href="/">
-          <i className="fa-brands fa-instagram-square" />
-        </a>
-        <a href="/">
-          <i className="fa-brands fa-youtube-square" />
-        </a>
+        <span className="socialmedia">
+          <a href="/">
+            <i className="fa-brands fa-facebook-square" />
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-instagram-square" />
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-youtube-square" />
+          </a>
+        </span>
       </div>
     </div>
   );
